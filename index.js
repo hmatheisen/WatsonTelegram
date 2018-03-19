f4a908b9ba7998691f61d8fb57c6d342c127521c
hmatheisen <henry.mthsn@gmail.com>
2018-03-19 19:39:42 +0100

    app listens on on port 8080 now

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const Conversation = require('watson-developer-cloud/conversation/v1');
+const app = require('http').createServer();
+
+app.listen(8080);
+
 require('dotenv').config();
 
 const token = process.env.TELEGRAM_TOKEN;
@@ -14,20 +18,22 @@ const conversation = new Conversation({
 });
 
 bot.on('message', (msg) => {
-    
+
     const chatId = msg.chat.id;
 
-    const payload = {
-        workspace_id: workspace_id,
-        input: msg || {}
-    }
-
-    conversation.message(payload, (err, response) => {
-        if (err) {
-            console.log('error: ', err);
-        } else {
-            bot.sendMessage(chatId, response.output.text[0]);
+        const payload = {
+            workspace_id: workspace_id,
+            input: msg || {}
         }
-    });
+    
+        conversation.message(payload, (err, response) => {
+            if (err) {
+                console.log('error: ', err);
+            } else {
+                console.log(response.intents[0].confidence);
+                
+                bot.sendMessage(chatId, response.output.text[0]);
+            }
+        });
 
 });
